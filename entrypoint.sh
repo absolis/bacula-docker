ef92707c7435d665bf40ca97377bdf2bc9024dd0
Alex Moiseenko <brainsam@yandex.ru>
2016-03-07 15:06:11 +0400

    Fixed dummy errors in batch

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,30 +2,29 @@
 
 #configure DB_PORT on default
 
-if [[-z  $DB_PORT]] && [[-n $DB_HOST]] && [[$DB_HOST != localhost]] && [[$DB_HOST != 127.0.0.1]]; then
-	case ${DB_TYPE}
+if [ -z $DB_PORT ] && [ -n $DB_HOST ] && [ $DB_HOST != localhost ] && [ $DB_HOST != 127.0.0.1 ]; then
+	case ${DB_TYPE} in
 		mysql)
-			$DB_PORT=3306
+			export DB_PORT=3306
 			;;
 		postgresql)
-			$DB_PORT=5432
+			export DB_PORT=5432
 			;;
 	esac
 fi
 
 # Change config settings on bacula-dir.conf
 
-sed -i 's/^\s*\(dbname\s*=\s*\).*\(user\s*=\s*\).*\(password\s*=\s*\).*$/
-\1$DB_NAME \2$DB_USER \3$DB_PASS/' /opt/bacula/etc/bacula-dir.conf
+sed -i "s/^\s*\(dbname\s*=\s*\).*\(user\s*=\s*\).*\(password\s*=\s*\).*$/\1$DB_NAME \2$DB_USER \3$DB_PASS/" /opt/bacula/etc/bacula-dir.conf
 
-grep -q 'DB Address' /opt/bacula/etc/bacula-dir.conf &&  sed -i 's/\(DB\sAddress\s*=\s*\).*/\1$DB_HOST/' /opt/bacula/etc/bacula-dir.conf || sed -i 's/\(^.*dbname.*$\)/\1\nDB Address = $DB_HOST/' /opt/bacula/etc/bacula-dir.conf
+grep -q 'DB Address' /opt/bacula/etc/bacula-dir.conf &&  sed -i "s/\(DB\sAddress\s*=\s*\).*/\1$DB_HOST/" /opt/bacula/etc/bacula-dir.conf || sed -i "s/\(^.*dbname.*$\)/\1\nDB Address = $DB_HOST/" /opt/bacula/etc/bacula-dir.conf
 
-grep -q 'DB Port' /opt/bacula/etc/bacula-dir.conf &&  sed -i 's/\(DB\sPort\s*=\s*\).*/\1$DB_PORT/' /opt/bacula/etc/bacula-dir.conf || sed -i 's/\(^.*DB\sAddress.*$\)/\1\nDB Port = $DB_PORT/' /opt/bacula/etc/bacula-dir.conf
+grep -q 'DB Port' /opt/bacula/etc/bacula-dir.conf &&  sed -i "s/\(DB\sPort\s*=\s*\).*/\1$DB_PORT/" /opt/bacula/etc/bacula-dir.conf || sed -i "s/\(^.*DB\sAddress.*$\)/\1\nDB Port = $DB_PORT/" /opt/bacula/etc/bacula-dir.conf
 
 
 
 # add mysql creation of db if not exists
-$db_name = $DB_NAME
+export db_name=$DB_NAME
 case ${DB_TYPE} in
 	mysql)
 		/opt/bacula/etc/create_bacula_database mysql -u $DB_USER ${DB_PASS:+"-p $DB_PASS"} ${DB_HOST:+"-h $DB_HOST"} ${DB_PORT:+"-P $DB_PORT"}
@@ -39,22 +38,23 @@ case ${DB_TYPE} in
 		/opt/bacula/etc/create_bacula_database sqlite
 		/opt/bacula/etc/make_bacula_tables sqlite 
 		;;
-
+esac
 # Change message settings
 
-sed -i '/s/Messages\s{.*!skipped$^}/
-Messages {
-  Name = Standard
-  mailcommand = "/home/bacula/bin/bsmtp -h ${SMTP_HOST:-localhost}
-                -f \"\(Bacula\) %r\"
-                -s \"Bacula: %t %e of %c %l\" %r"
-  operatorcommand = "/home/bacula/bin/bsmtp -h localhost
-                -f \"\(Bacula\) %r\"
-                -s \"Bacula: Intervention needed for %j\" %r"
-  Mail = ${ADMIN_EMAIL} = all, !skipped, !terminate
-  append = "/home/bacula/bin/log" = all, !skipped, !terminate
-  operator = ${ADMIN_EMAIL} = mount
-  console = all, !skipped, !saved/
-}'
+Messages="Messages {\n"
+Messages="$Messages   Name = Standard\n"
+Messages="$Messages   mailcommand = \"/home/bacula/bin/bsmtp -h ${SMTP_HOST:-localhost}\n"
+Messages="$Messages                 -f \\\"\\(Bacula\\) %r\\\"\n"
+Messages="$Messages                 -s \\\"Bacula: %t %e of %c %l\\\" %r\"\n"
+Messages="$Messages   operatorcommand = \"/home/bacula/bin/bsmtp -h localhost\n"
+Messages="$Messages                 -f \\\"\\(Bacula\\) %r\\\"\n"
+Messages="$Messages                 -s \\\"Bacula: Intervention needed for %j\\\" %r\"\n"
+Messages="$Messages   Mail = ${ADMIN_EMAIL} = all, !skipped, !terminate\n"
+Messages="$Messages   append = \"/home/bacula/bin/log\" = all, !skipped, !terminate\n"
+Messages="$Messages   operator = ${ADMIN_EMAIL} = mount\n"
+Messages="$Messages   console = all, !skipped, !saved/\n"
+Messages="$Messages }\n"
+
+sed -i '/s/Messages\s{.*!skipped$^}/$Messages/'
 
 bacula start
