e3cfaf048df508313f586b7cd02eeb97a8b75d2a
Alex Moiseenko <brainsam@yandex.ru>
2016-03-10 17:41:43 +0400

    removed sqlite and postgres options

--- Dockerfile
+++ Dockerfile
@@ -3,42 +3,37 @@ FROM debian:latest
 MAINTAINER Alex Moiseenko <brainsam@yandex.ru>
 
 ENV DIR_NAME=bacula-dir
-ENV DB_TYPE=mysql
 ENV DB_NAME=bacula
 ENV DB_USER=bacula
 ENV DB_PASS=password
-ENV ADMIN_EMAIL=your@address.com
 ENV PATH /opt/bacula/etc:$PATH
 
-RUN apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y wget gcc g++ make file git libmysqlclient-dev mysql-client sqlite3 bacula-common-pgsql libpq-dev
-
-RUN mkdir /opt/bacula
-RUN git clone http://git.bacula.org/bacula bacula 
-
-RUN cd bacula/bacula && ./configure \
-        --enable-smartalloc \
-        --enable-batch-insert \
-        --with-mysql \
-		--with-sqlite \
-		--with-postgresql \
-        --sbindir=/opt/bacula/bin \
-        --sysconfdir=/opt/bacula/etc \
-        --with-pid-dir=/opt/bacula/working \
-        --with-subsys-dir=/opt/bacula/working \
-        --with-working-dir=/opt/bacula/working \
-		--disable-build-stored \
-		--with-x=NO && \
-		
-make && make install
+RUN apt-get update && \
+DEBIAN_FRONTEND=noninteractive apt-get install -y wget gcc g++ make file git libmysqlclient-dev mysql-client && \
+mkdir /opt/bacula && \
+git clone http://git.bacula.org/bacula bacula && \
+cd bacula/bacula && \
+./configure \
+	--enable-smartalloc \
+	--enable-batch-insert \
+	--with-mysql \
+	--sbindir=/opt/bacula/bin \
+	--sysconfdir=/opt/bacula/etc \
+	--with-pid-dir=/opt/bacula/working \
+	--with-subsys-dir=/opt/bacula/working \
+	--with-working-dir=/opt/bacula/working \
+	--disable-build-stored \
+	--with-x=NO && \
+make && make install && \
 
 # Add custom user config catalog
-RUN mkdir /opt/bacula/etc/conf.d /opt/bacula/data
-RUN echo "@|\"sh -c 'for f in /opt/bacula/etc/conf.d/*.conf ; do echo @\$\{f\} ; done'\"" >> /opt/bacula/etc/bacula-dir.conf
+mkdir /opt/bacula/etc/conf.d /opt/bacula/data && \
+echo "@|\"sh -c 'for f in /opt/bacula/etc/conf.d/*.conf ; do echo @\$\{f\} ; done'\"" >> /opt/bacula/etc/bacula-dir.conf && \
 
 # Clean system from useless files
-RUN apt-get remove wget gcc g++ make file git libmysqlclient-dev sqlite3 bacula-common-pgsql libpq-dev -y
-RUN apt-get clean all
-RUN rm -rf bacula
+apt-get remove wget gcc g++ make file git libmysqlclient-dev sqlite3 bacula-common-pgsql libpq-dev -y && \
+apt-get clean all && \
+rm -rf bacula
 
 VOLUME /opt/bacula/etc/conf.d
 VOLUME /opt/bacula/data
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,48 +1,29 @@
 #!/bin/sh
 
 #configure DB_PORT on default
-
-if [ -z $DB_PORT ] && [ -n $DB_HOST ] && [ $DB_HOST != localhost ] && [ $DB_HOST != 127.0.0.1 ]; then
-	case ${DB_TYPE} in
-		mysql)
-			export DB_PORT=3306
-			;;
-		postgresql)
-			export DB_PORT=5432
-			;;
-	esac
+if [[ -z $DB_PORT && -n $DB_HOST && $DB_HOST != localhost && $DB_HOST != 127.0.0.1 ]]; 
+then
+	export DB_PORT=3306
 fi
 
 # Change config settings on bacula-dir.conf
-
 sed -i "s/\(Director\s*{\s*\n\s*Name\s=\s\).*$/\1$DIR_NAME/" /opt/bacula/etc/bacula-dir.conf
-
-sed -i "s/\(^\s*dbname\s*=\s*\).*\s*\(user\s*=\s*\).*\s*\(password\s*=\s*\).*$/\1$DB_NAME\ \2$DB_USER\ \3$DB_PASS/" /opt/bacula/etc/bacula-dir.conf
-
-grep -q 'DB Address' /opt/bacula/etc/bacula-dir.conf &&  sed -i "s/\(DB\sAddress\s*=\s*\).*/\1$DB_HOST/" /opt/bacula/etc/bacula-dir.conf || sed -i "s/\(^.*dbname.*$\)/\1\n  DB Address = $DB_HOST/" /opt/bacula/etc/bacula-dir.conf
-
-grep -q 'DB Port' /opt/bacula/etc/bacula-dir.conf &&  sed -i "s/\(DB\sPort\s*=\s*\).*/\1$DB_PORT/" /opt/bacula/etc/bacula-dir.conf || sed -i "s/\(^.*DB\sAddress.*$\)/\1\n  DB Port = $DB_PORT/" /opt/bacula/etc/bacula-dir.conf
-
-
+sed -i "s/\(^\s*dbname\s*=\s*\).*\s*\(user\s*=\s*\).*\s*\(password\s*=\s*\).*$/\1$DB_NAME;\ \2$DB_USER;\ \3\"$DB_PASS\"/" /opt/bacula/etc/bacula-dir.conf
+grep -q 'DB Address' /opt/bacula/etc/bacula-dir.conf && \
+	sed -i "s/\(DB\sAddress\s*=\s*\).*/\1$DB_HOST/" /opt/bacula/etc/bacula-dir.conf || \
+		sed -i "s/\(^.*dbname.*$\)/\1\n  DB Address = $DB_HOST/" /opt/bacula/etc/bacula-dir.conf
+grep -q 'DB Port' /opt/bacula/etc/bacula-dir.conf && \
+	sed -i "s/\(DB\sPort\s*=\s*\).*/\1$DB_PORT/" /opt/bacula/etc/bacula-dir.conf || \
+		sed -i "s/\(^.*DB\sAddress.*$\)/\1\n  DB Port = $DB_PORT/" /opt/bacula/etc/bacula-dir.conf
 
 # add mysql creation of db if not exists
-export db_name=$DB_NAME
-case ${DB_TYPE} in
-	mysql)
-		/opt/bacula/etc/create_bacula_database mysql -u $DB_USER ${DB_PASS:+-p$DB_PASS} ${DB_HOST:+-h $DB_HOST} ${DB_PORT:+-P $DB_PORT}
-		/opt/bacula/etc/make_bacula_tables mysql -u $DB_USER ${DB_PASS:+-p$DB_PASS} ${DB_HOST:+-h $DB_HOST} ${DB_PORT:+-P $DB_PORT}
-		;;
-	postgresql)
-		/opt/bacula/etc/create_bacula_database postgresql -U $DB_USER ${DB_PASS:+-W$DB_PASS} ${DB_HOST:+-h $DB_HOST} ${DB_PORT:+-p $DB_PORT}
-		/opt/bacula/etc/make_bacula_tables postgresql -U $DB_USER ${DB_PASS:+-W$DB_PASS} ${DB_HOST:+-h $DB_HOST} ${DB_PORT:+-p $DB_PORT}
-		;;
-	sqlite)
-		/opt/bacula/etc/create_bacula_database sqlite
-		/opt/bacula/etc/make_bacula_tables sqlite 
-		;;
-esac
-# Change message settings
+export db_name="$DB_NAME"
+/opt/bacula/etc/create_bacula_database mysql -u $DB_USER ${DB_PASS:+-p$DB_PASS} ${DB_HOST:+-h $DB_HOST} ${DB_PORT:+-P $DB_PORT}
+/opt/bacula/etc/make_bacula_tables mysql -u $DB_USER ${DB_PASS:+-p$DB_PASS} ${DB_HOST:+-h $DB_HOST} ${DB_PORT:+-P $DB_PORT}
 
+# Change message settings
+if [[ -z $ADMIN_EMAIL ]];
+then
 Messages="Messages {\n"
 Messages="$Messages   Name = Standard\n"
 Messages="$Messages   mailcommand = \"/home/bacula/bin/bsmtp -h ${SMTP_HOST:-localhost}\n"
@@ -56,7 +37,6 @@ Messages="$Messages   append = \"/home/bacula/bin/log\" = all, !skipped, !termin
 Messages="$Messages   operator = ${ADMIN_EMAIL} = mount\n"
 Messages="$Messages   console = all, !skipped, !saved/\n"
 Messages="$Messages }\n"
-
 sed -i 's/Messages\s*{\s*Name = Standard.*!skipped$^}/$Messages/' /opt/bacula/etc/bacula-dir.conf
 
-bacula start -d
+exec /opt/bacula/bin/bacula-dir -d /opt/bacula/etc/bacula-dir.conf
